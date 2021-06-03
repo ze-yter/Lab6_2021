b5aac4a1e8e62183308cd9812dcdd9e0273823a4
Nastya-Grigorieva <85290064+Nastya-Grigorieva@users.noreply.github.com>
2021-06-03 18:33:38 +0500

    Add files via upload

---
+++ Игра Урфу/FPF/five.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace FivePlusFive
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(130, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 29);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Score:0";
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 313);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "5+5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
